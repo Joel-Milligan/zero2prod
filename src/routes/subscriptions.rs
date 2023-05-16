74dbdc6a00dbd9c51539d0a610110208410b65e0
Joel Milligan <joelmilligan24@gmail.com>
2023-05-16 20:40:31 +0800

    Clean up logs, so errors only show up once

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -146,11 +146,7 @@ pub async fn insert_subscriber(
         Utc::now()
     )
     .execute(transaction)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {e:?}");
-        e
-    })?;
+    .await?;
     Ok(subscriber_id)
 }
 
@@ -171,10 +167,7 @@ pub async fn store_token(
     )
     .execute(transaction)
     .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {e:?}");
-        StoreTokenError(e)
-    })?;
+    .map_err(|e| StoreTokenError(e))?;
     Ok(())
 }
 
