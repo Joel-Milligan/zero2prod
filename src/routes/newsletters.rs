e78d91c910364ad80ea01fb43164ff58a291b177
Joel Milligan <joelmilligan24@gmail.com>
2023-08-01 22:20:25 +0800

    Fix clippy issues

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -177,7 +177,7 @@ async fn validate_credentials(
     let mut user_id = None;
     let mut expected_password_hash = Secret::new("$argon2id$v=19$m=15000,t=2,p=1$gZiV/M1gPc22ElAH/Jh1Hw$CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno".to_string());
     if let Some((stored_user_id, stored_password_hash)) =
-        get_stored_credentials(&credentials.username, &pool)
+        get_stored_credentials(&credentials.username, pool)
             .await
             .map_err(PublishError::UnexpectedError)?
     {
@@ -203,7 +203,7 @@ fn verify_password_hash(
     expected_password_hash: Secret<String>,
     password_candiate: Secret<String>,
 ) -> Result<(), PublishError> {
-    let expected_password_hash = PasswordHash::new(&expected_password_hash.expose_secret())
+    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
         .context("Failed to parse hash in PHC string format.")
         .map_err(PublishError::UnexpectedError)?;
 
