1241c148866b9241968f1986a6874463121928cd
Joel Milligan <joelmilligan24@gmail.com>
2023-08-02 14:51:59 +0800

    Introduce ability to login

--- src/startup.rs
+++ src/startup.rs
@@ -7,7 +7,9 @@ use tracing_actix_web::TracingLogger;
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
+use crate::routes::{
+    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+};
 
 pub struct Application {
     port: u16,
@@ -75,7 +77,10 @@ pub fn run(
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .route("/", web::get().to(home))
             .route("/health_check", web::get().to(health_check))
+            .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
