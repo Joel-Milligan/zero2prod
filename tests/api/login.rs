18f636b4e88947fdf858233a2ed64bd0f0eadeaf
Joel Milligan <joelmilligan24@gmail.com>
2023-08-03 13:15:38 +0800

    Expire cookie after first use

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -2,10 +2,9 @@ use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
 async fn an_error_flash_message_is_set_on_failure() {
-    // Arrange
     let app = spawn_app().await;
 
-    // Act
+    // Part 1 - Try to login
     let login_body = serde_json::json!({
         "username": "random-username",
         "password": "random-password"
@@ -14,7 +13,14 @@ async fn an_error_flash_message_is_set_on_failure() {
     let response = app.post_login(&login_body).await;
     let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
 
-    // Assert
     assert_is_redirect_to(&response, "/login");
     assert_eq!(flash_cookie.value(), "Authentication failed.");
+
+    // Part 2 - Follow the redirect
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains(r#"<p><i>Authentication failed.</i></p>"#));
+
+    // Part 3 - Reload the login page
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains(r#"<p><i>Authentication failed.</i></p>"#));
 }
