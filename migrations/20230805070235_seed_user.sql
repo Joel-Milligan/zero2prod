c13a2505dc1c1252b33618d090d43f2d2dc79e23
Joel Milligan <joelmilligan24@gmail.com>
2023-08-05 15:19:05 +0800

    Create seed user

---
+++ migrations/20230805070235_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    '4eed0f79-a3ed-4dd8-b264-ee725905234e',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$1xiK/t0lISE/k940A3nDUw$r7TZpx3M7dyUSwT/Us5uDwJimnpIvLPl3EMiwDl6yQA'
+);
